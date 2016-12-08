2c3e0df4eb451c6961996e782336c44f71b95503
Katrina Tollin <email": null>
2016-12-08 16:42:00 -0600

    hide env file

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,3 +9,19 @@ post '/receive_sms' do
   end
   response.to_xml
 end
+
+post '/send_sms' do
+  to = params["to"]
+  message = params["body"]
+
+  client = Twilio::REST::Client.new(
+    ENV["TWILIO_ACCOUNT_SID"],
+    ENV["TWILIO_AUTH_TOKEN"]
+    )
+
+  client.messages.create(
+    to: to,
+    from: "+15122336886",
+    body: message
+    )
+end
